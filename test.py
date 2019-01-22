113120da9491df04962daa29466d1a909273bb0c
oldmalebird <lgnlgnlgnlgn@gmail.com>
2019-01-22 11:33:24 +0800

    更新20190122价格

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-import chardet
-print(chardet.detect(b'Hello, world!')['encoding'])
-# print(chardet.detect(b'价格不错 大黄瓜种植户却很受伤.txt')['encoding'])
-# print(chardet.detect(b'市场）元宵后蔬菜市场：量足价跌 交易​繁忙.txt')['encoding'])
-a = "D:/Data/小程序/北方蔬菜报/2017/20170106/北方蔬菜报2017.01.06/1热点关注/数九隆冬 葡萄飘香.txt"
-b = "D:/Data/小程序/北方蔬菜报/2017/20170811/2017.8.11/170811黄瓜死棵重? 区别来防治.txt"
-print(type(a))
-f = open(a)
-print(f.read())
-#print(chardet.detect(f.read())['encoding'])
-# f = open(b)
-# print(f.read())
-# print(chardet.detect(f.read())['encoding'])
+# -*-coding:utf-8-*-
+import os
 
-# f = open(
-#     "D:\\Data\\小程序\\北方蔬菜报\\2017\\20170217\\北方蔬菜报2017.02.17\\市场\\市场）元宵后蔬菜市场：量足价跌 交易繁忙.txt"
-# )
+in_path = r"D:\Data\小程序\北方蔬菜报\worddict"
+fin_path = [fname for fname in os.listdir(in_path) if fname[-4:] == ".txt"]
+print(fin_path)
+veg_words = []
+for f in fin_path:
+    print("f: ", f)
+    lines = open(os.path.join(in_path, f), 'r', encoding='utf-8')
+    #lines = open(os.path.join(in_path, f))
+    for line in lines:
+        veg_words.append(line.strip())
+    print('现在收录的农业用词有：', len(veg_words))
+    lines.close()
 
-# address = "D:\Data\小程序\北方蔬菜报\2017\20170217\北方蔬菜报2017.02.17\市场\市场）元宵后蔬菜市场：量足价跌 交易​繁忙.txt"
-# start = 'r"'
-# end = '"'
-# f = open(start + address + end, 'rb')
-# print(chardet.detect(f.read())['encoding'])
+veg_words = set(veg_words)
+print('去重后的农业用词有：', len(veg_words))  #3.7w
+
+# 写入veg_dict.txt自定义词库
+veg_dict = open(r"D:\Data\小程序\北方蔬菜报\veg_dict.txt", 'w')
+veg_dict.write('\n'.join(veg_words))
+veg_dict.close()
